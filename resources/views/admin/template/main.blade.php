503f152812d5a3e87e0405b0887454e716815dfa
camilo villegas <cagudelovillegas@gmail.com>
2018-09-12 00:32:30 -0500

    css arreglado

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -7,8 +7,8 @@
     <title>@yield('title','default')</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/reset.css"/>
-    <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    <link rel="stylesheet" type="text/css" href={!! asset("css/reset.css")!!}>
+    <link rel="stylesheet" type="text/css" href={!! asset("css/main.css")!!}>
   </head>
   <body>
     @include('admin.template.partials.nav')
